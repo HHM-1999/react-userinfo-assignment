1d72ac5ecce85b4d89ae3040b4e0f3dd872c5790
HHM-1999 <hhm639069@gmail.com>
2025-05-13 15:43:11 +0600

    Add UserInfo component

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './Components/Home'
+
+function App() {
+
+  return (
+    <>
+        <div className="main-site">
+          <Home />
+        </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Personal Dashboard</div>
+  )
+}
+
+export default Home
