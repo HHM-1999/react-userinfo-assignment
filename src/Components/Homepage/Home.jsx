955e62e84edf16739711ea9e9346c7a1cce8eb7a
HHM-1999 <hhm639069@gmail.com>
2025-05-13 16:14:03 +0600

    create component for homepage

---
+++ src/Components/Homepage/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className='deshboardSection'>
+        <h1>Personal Dashboard</h1>
+      </div>
+    </div>
+
+  )
+}
+
+export default Home
